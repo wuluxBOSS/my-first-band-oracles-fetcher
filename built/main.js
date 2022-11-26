526f2546df50818c1f37f999c84595724f1880dc
wuluxBOSS <damian12msi@gmail.com>
2022-11-26 14:18:03 +0100

    difficult problem

--- built/main.js
+++ built/main.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -34,19 +35,31 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var axios = Promise.resolve().then(function () { return require('axios'); });
-// var axios = Promise.resolve(import('axios')).then(function () { return import('axios'); });
+Object.defineProperty(exports, "__esModule", { value: true });
+var axios_1 = require("axios");
 function getBTCprice() {
     return __awaiter(this, void 0, void 0, function () {
-        var res, obj;
+        var res, error_1;
         return __generator(this, function (_a) {
             switch (_a.label) {
-                case 0: return [4 /*yield*/, axios.get('https://api.coingecko.com/api/v3/coins/bitcoin')];
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    return [4 /*yield*/, axios_1.default.get('https://api.coingecko.com/api/v3/coins/bitcoin')];
                 case 1:
                     res = _a.sent();
-                    obj = res.data.market_data.current_price.usd;
-                    console.log("BTC price in USD: ".concat(obj));
-                    return [2 /*return*/];
+                    // let obj= res.data.market_data.current_price.usd; 
+                    // console.log(`BTC price in USD: ${obj}`);
+                    console.log(res);
+                    return [2 /*return*/, res];
+                case 2:
+                    error_1 = _a.sent();
+                    // if (axios.isAxiosError(error)) {
+                    //   console.log('error message: ', error.message);
+                    //   return error.message;
+                    // } else {
+                    console.log('unexpected error: ', error_1);
+                    return [2 /*return*/, 'An unexpected error occurred'];
+                case 3: return [2 /*return*/];
             }
         });
     });
